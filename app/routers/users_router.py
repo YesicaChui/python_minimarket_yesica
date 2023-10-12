ce405e3cba796d03e3a388d77a561fd5fca8e149
YesicaChui <yesicachuic@gmail.com>
2023-10-11 19:00:00 -0500

    protegiendo las rutas de usuarios

--- app/routers/users_router.py
+++ app/routers/users_router.py
@@ -1,6 +1,7 @@
 from app import api
 from flask_restx import Resource
 from flask import request
+from flask_jwt_extended import jwt_required
 from app.schemas.users_schema import UserRequestSchema
 from app.controllers.users_controller import UserController
 
@@ -15,14 +16,17 @@ schema_request = UserRequestSchema(user_ns)
 
 
 @user_ns.route('')
+@user_ns.doc(security= 'Bearer')
 class Users(Resource):
+    @jwt_required()
     @user_ns.expect(schema_request.all())
     def get(self):
         ''' Listar todos los usuarios '''
         query_params = schema_request.all().parse_args()
         controller = UserController()
         return controller.fetch_all(query_params)
-
+    
+    @jwt_required()
     @user_ns.expect(schema_request.create(), validate=True)
     def post(self):
         ''' Creación de un usuario '''
@@ -31,18 +35,22 @@ class Users(Resource):
 
 
 @user_ns.route('/<int:id>')
+@user_ns.doc(security= 'Bearer')
 class UserById(Resource):
+    @jwt_required()
     def get(self, id):
         ''' Obtener un usuario por su id '''
         controller = UserController()
         return controller.find_by_id(id)
 
+    @jwt_required()
     @user_ns.expect(schema_request.update(), validate=True)
     def patch(self, id):
         ''' Actualizar un usuario por su id, enviando el objeto parcial '''
         controller = UserController()
         return controller.update(id, request.json)
-
+    
+    @jwt_required()
     def delete(self, id):
         ''' Inhabilitar un usuario por su id '''
         controller = UserController()
